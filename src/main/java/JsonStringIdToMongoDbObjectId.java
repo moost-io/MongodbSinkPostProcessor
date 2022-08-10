931b1cd0550723a2cd1bf20b4162621c4fcd3950
Lucas Senn <lucas.senn@moost.io>
2022-08-10 17:20:03 +0200

    initial commit

---
+++ src/main/java/JsonStringIdToMongoDbObjectId.java
@@ -0,0 +1,20 @@
+import com.mongodb.kafka.connect.sink.MongoSinkTopicConfig;
+import com.mongodb.kafka.connect.sink.converter.SinkDocument;
+import com.mongodb.kafka.connect.sink.processor.PostProcessor;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.bson.BsonObjectId;
+import org.bson.types.ObjectId;
+
+public class JsonStringIdToMongoDbObjectId extends PostProcessor {
+
+    public JsonStringIdToMongoDbObjectId(MongoSinkTopicConfig config) {
+        super(config);
+    }
+
+    @Override
+    public void process(SinkDocument doc, SinkRecord orig) {
+            doc.getValueDoc().ifPresent(vd ->
+                    vd.append("_id", doc.getValueDoc().get().get("id").asObjectId())
+            );
+    }
+}
