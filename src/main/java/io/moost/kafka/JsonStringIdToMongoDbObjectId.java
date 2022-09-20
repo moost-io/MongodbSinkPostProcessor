d0d697dd12d3de93dd7cd5510990d3f339d42819
Lucas Senn <lucas.senn@moost.io>
2022-09-20 16:40:43 +0200

    First final version

--- src/main/java/io/moost/kafka/JsonStringIdToMongoDbObjectId.java
+++ src/main/java/io/moost/kafka/JsonStringIdToMongoDbObjectId.java
@@ -4,6 +4,8 @@ import com.mongodb.kafka.connect.sink.MongoSinkTopicConfig;
 import com.mongodb.kafka.connect.sink.converter.SinkDocument;
 import com.mongodb.kafka.connect.sink.processor.PostProcessor;
 import org.apache.kafka.connect.sink.SinkRecord;
+import org.bson.BsonObjectId;
+import org.bson.types.ObjectId;
 
 
 public class JsonStringIdToMongoDbObjectId extends PostProcessor {
@@ -13,8 +15,10 @@ public class JsonStringIdToMongoDbObjectId extends PostProcessor {
 
     @Override
     public void process(SinkDocument doc, SinkRecord orig) {
-        doc.getValueDoc().ifPresent(vd ->
-                vd.append("_id", vd.getString("id").asObjectId())
+        doc.getValueDoc().ifPresent(vd -> {
+                    vd.append("_id", new BsonObjectId(new ObjectId(vd.getString("id").getValue())));
+                    vd.remove("id");
+                }
         );
     }
 }
